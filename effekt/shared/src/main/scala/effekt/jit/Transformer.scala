61bd143116adfc280bbc7d13d4e5bd22e126bc83
Marcial Gaißert <gaisseml@studi.informatik.uni-stuttgart.de>
2022-09-22 18:31:21 +0200

    JIT: Add basic support for `String`s

--- effekt/shared/src/main/scala/effekt/jit/Transformer.scala
+++ effekt/shared/src/main/scala/effekt/jit/Transformer.scala
@@ -7,6 +7,7 @@ import effekt.jit.BlockNumbering.*
 import effekt.machine
 import effekt.machine.{Declaration, analysis}
 
+import java.nio.charset.StandardCharsets
 import scala.annotation.targetName
 import scala.collection.mutable
 import scala.collection.immutable
@@ -153,6 +154,11 @@ object Transformer {
         emit(ConstDouble(outs(RegisterType.Double).head, value));
         emitInlined(block)
       }
+      case machine.LiteralUTF8String(out, value, rest) => {
+        val (_, RegList(outs), block) = transformInline(machine.Clause(List(out), rest));
+        emit(ConstString(outs(RegisterType.String).head, new String(value, StandardCharsets.UTF_8))); // TODO: Escape or encode somehow
+        emitInlined(block)
+      }
       case machine.NewStack(name, frame, rest) => {
         val (closesOver, _, target) = transformClosure(frame);
         val (_, RegList(outs), restBlock) = transformInline(machine.Clause(List(name), rest));
@@ -180,6 +186,7 @@ object Transformer {
       case machine.Negative(alternatives) => Type.Codata(PC.codatas.indexOfOrInsert(alternatives))
       case machine.Type.Int() => Type.Integer()
       case machine.Type.Double() => Type.Double()
+      case machine.Type.String() => Type.String()
       case machine.Type.Stack() => Type.Continuation()
   }
 
